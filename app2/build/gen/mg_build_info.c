055c80120054451f9445acb8dbe11a75e24bd1b9
Glioce <0>
2020-06-01 13:10:03 -0500

    segundo ejemplo

---
+++ app2/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20200530-005810/2.17.0-gc31a745";
+const char *mg_build_timestamp = "2020-05-30T00:58:10Z";
+const char *mg_build_version = "2.17.0";
